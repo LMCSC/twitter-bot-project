959fdaf566a60c8c69a4218142cd612f25d845f5
John Chin-Jew <jchinjew@gmail.com>
2018-04-22 16:55:52 -0700

    Lesson #1: JavaScript basics

---
+++ js-basics.js
@@ -0,0 +1,24 @@
+// Variables and data types
+var myNumber = 5 * -6.3
+var myString = "Hey"
+var myBool   = false
+var myList   = [-13.4, myNumber, "three", myBool]
+
+// Printing
+console.log("myNumber: ", myNumber)
+console.log("myString: ", myString)
+console.log("myBool:   ",   myBool)
+console.log("myList:   ",   myList)
+
+// Conditional logic
+var condition1 = false
+var condition2 = false
+var condition3 = true
+
+if (condition1) {
+  console.log("Condition 1 is true")
+} else if (condition2) {
+  console.log("Condition 2 is true")
+} else {
+  console.log("Condition 3 is true")
+}
