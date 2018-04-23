8b6305e8c682dd56c789c2b6499a8eb47bb17a9a
John Chin-Jew <jchinjew@gmail.com>
2018-04-22 18:21:42 -0700

    Update first-bot.js with comments

--- Module 2/first-bot.js
+++ Module 2/first-bot.js
@@ -1,7 +1,9 @@
 console.log("Bot started!")
 
+// Import Twit package
 var twit = require("twit")
 
+// Authentication
 var t = new twit({
   consumer_key:        '...',
   consumer_secret:     '...',
@@ -9,12 +11,14 @@ var t = new twit({
   access_token_secret: '...',
 })
 
+// Basic tweet
 t.post(
   "statuses/update",
   { status: "Testing..." },
   function(err, data, resp) { if (err) console.log("Error") }
 )
 
+// To be used later...
 // var type = "statuses/update"
 // var tweet = { status: "..." }
 // function tweeted(err, data, resp) {
